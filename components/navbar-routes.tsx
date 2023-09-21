2274cfc1df87f4dcedce11c4f1b82d275b40baec
youneshenniwrites <youneshenniwrites@gmail.com>
2023-09-21 19:19:31 +0100

    implement teacher mode

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,10 +1,33 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
 
 export const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isCoursePage = pathname?.includes("/courses");
+
   return (
     <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isCoursePage ? (
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
       <UserButton afterSignOutUrl="/" />
     </div>
   );
