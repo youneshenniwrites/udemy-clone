aac13487c5a56eb4cafdcce262ba31dbf17f82aa
youneshenniwrites <youneshenniwrites@gmail.com>
2023-09-23 20:43:07 +0100

    implement course id dynamic header

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,50 @@
-export default function CourseIdPage({
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function CourseIdPage({
   params,
 }: {
   params: { courseId: string };
 }) {
-  return <div>Course Id: {params.courseId}</div>;
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+      userId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex flex-col gap-y-2">
+        <h1 className="text-2xl font-medium">Course setup</h1>
+        <span className="text-sm text-slate-700">
+          Complete all fields {completionText}
+        </span>
+      </div>
+    </div>
+  );
 }
