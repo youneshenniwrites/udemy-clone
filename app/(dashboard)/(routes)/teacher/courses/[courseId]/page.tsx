26862a599ec36c481d0a356615a5da23c70fbce6
youneshenniwrites <youneshenniwrites@gmail.com>
2023-09-24 16:01:17 +0100

    create a custom icon badge component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 
 export default async function CourseIdPage({
@@ -44,6 +46,14 @@ export default async function CourseIdPage({
         <span className="text-sm text-slate-700">
           Complete all fields {completionText}
         </span>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
